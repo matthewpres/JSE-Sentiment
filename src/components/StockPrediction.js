08add875664a09440982151804d3ebe63c107b9c
matthewpres <bay.violet2580@eagereverest.com>
2025-02-10 15:17:13 -0500

    Create StockPrediction.js

---
+++ src/components/StockPrediction.js
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="min-h-screen bg-gray-50 py-8">
+            <StockPrediction />
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
